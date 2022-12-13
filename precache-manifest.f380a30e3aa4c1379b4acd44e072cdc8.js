e5b1d59bc6b646f8e9c621c5beeb6c045fcedb9e
KamandeJohn <kamaajohn2015@gmail.com>
2022-12-14 00:03:04 +0300

    Updates

---
+++ precache-manifest.f380a30e3aa4c1379b4acd44e072cdc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed80a16a7573579ddc83",
+    "url": "/starting-react/static/css/main.9cbf02bb.chunk.css"
+  },
+  {
+    "revision": "ed80a16a7573579ddc83",
+    "url": "/starting-react/static/js/main.ed80a16a.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/starting-react/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/starting-react/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "c3813ce0ca4262c77ac6",
+    "url": "/starting-react/static/js/runtime~main.c3813ce0.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/starting-react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2f635a157b54d8e4eeeeaea483f17e31",
+    "url": "/starting-react/index.html"
+  }
+];
